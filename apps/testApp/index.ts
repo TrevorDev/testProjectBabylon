ed0d8fbcf9d2d31cfcd34d02ea501b819ec61e00
Trevor Baron <trevorjbaron@gmail.com>
2018-07-05 00:22:07 -0700

    example app

---
+++ apps/testApp/index.ts
@@ -0,0 +1,51 @@
+import { VRExperienceHelper } from "babylonjs";
+
+var shell:any = (<any>window).shell
+
+var makeNotPickable = (mesh:BABYLON.AbstractMesh)=>{
+    mesh.isPickable = false;
+    mesh.getChildMeshes().forEach((m)=>{
+        makeNotPickable(m)
+    })
+}
+
+shell.registerApp({
+    name: "testApp", 
+    launch: async (windowAnchor:BABYLON.Mesh, vrHelper: VRExperienceHelper)=>{
+        // Get scene
+        var scene = windowAnchor.getScene();
+
+        // Load gltf model and add to scene
+        var container = await BABYLON.SceneLoader.LoadAssetContainerAsync("https://raw.githubusercontent.com/TrevorDev/gltfModels/master/facebook.glb", "", this.scene)
+        var loadedModel = container.createRootMesh()
+        makeNotPickable(loadedModel) // This needs to be done on large models to save on perf when doing ray collisions from controllers
+        loadedModel.position.y = 2
+        scene.addMesh(loadedModel, true)
+        // Any mesh created MUST have the windowAnchor as it's parent
+        loadedModel.parent = windowAnchor
+
+        // Create GUI button
+        var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 0.2, height: 0.2}, this.scene)
+        plane.position.y= 1
+        plane.parent = windowAnchor // set windowAnchor as parent
+        var guiTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane)
+        guiTexture
+        var guiPanel = new BABYLON.GUI.StackPanel()  
+        guiPanel.top = "0px"
+        guiTexture.addControl(guiPanel)
+        var button = BABYLON.GUI.Button.CreateSimpleButton("", "Click 🤣")
+        button.fontSize = 300
+        button.color = "white"
+        button.background = "#4AB3F4"
+        button.cornerRadius = 200
+        button.thickness = 20
+        button.onPointerClickObservable.add(()=>{
+            console.log("hit")
+        })
+        guiPanel.addControl(button)
+
+    }, 
+    dispose: async ()=>{
+
+    }
+})
--- index.ts
+++ index.ts
@@ -1,50 +1,54 @@
 import { Stage } from './src/stage'
 
-var main = async ()=>{
-    // Initialize full screen rendering
-    var stage = new Stage()
-    var scene = stage.scene
-    var canvas = stage.engine.getRenderingCanvas()
+class App {
+    constructor(public name:string, public launch:Function, public dispose:Function){
 
-    var env = scene.createDefaultEnvironment({})
-    env.setMainColor(BABYLON.Color3.FromHexString("#7f8c8d"))
-    env.skybox.scaling.scaleInPlace(10)
-
-    // Create basic world
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
-    camera.setTarget(BABYLON.Vector3.Zero())
-    camera.attachControl(canvas, true)
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
-    light.intensity = 0.7
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 1, scene)
-    sphere.position.set(0,3.5,0)
-
-    // Load GLTF
-    var root = await stage.importMesh("https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf")
-    root.position.y = 2
-    root.scaling.scaleInPlace(10)
-    scene.addMesh(root, true)
-
-    // Create GUI button
-    var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 1, height: 1}, this.scene)
-    plane.position.y= 1
-    var guiTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane)
-    guiTexture
-    var guiPanel = new BABYLON.GUI.StackPanel()  
-    guiPanel.top = "0px"
-    guiTexture.addControl(guiPanel)
-    var button = BABYLON.GUI.Button.CreateSimpleButton("", "Click 🤣")
-    button.fontSize = 300
-    button.color = "white"
-    button.background = "#4AB3F4"
-    button.cornerRadius = 200
-    button.thickness = 20
-    button.onPointerClickObservable.add(()=>{
-        console.log("hit")
-    })
-    guiPanel.addControl(button)
-    
-    // Setup vr
-    scene.createDefaultVRExperience({floorMeshes: [env.ground]})
+    }
 }
-main()
+class Shell {
+    private apps:Array<App> = []
+    constructor(public scene:BABYLON.Scene, public vrHelper:BABYLON.VRExperienceHelper){}
+    registerApp = (app:App)=>{
+        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 0.5, this.scene)
+        var anchor = new BABYLON.Mesh("", this.scene);
+        sphere.position.x = 1
+        sphere.rotation.y=Math.PI/4
+        this.apps.push(app)
+        app.launch(anchor, this.vrHelper)
+
+        var b = new BABYLON.PointerDragBehavior({dragPlaneNormal: new BABYLON.Vector3(0,1,0)})
+        sphere.addBehavior(b)
+        this.scene.onBeforeRenderObservable.add(()=>{
+            anchor.position.copyFrom(sphere.position)
+            anchor.rotation.copyFrom(anchor.rotation)
+        })
+    }
+}
+
+// Initialize full screen rendering
+var stage = new Stage()
+var scene = stage.scene
+var canvas = stage.engine.getRenderingCanvas()
+
+var env = scene.createDefaultEnvironment({})
+env.setMainColor(BABYLON.Color3.FromHexString("#7f8c8d"))
+env.skybox.scaling.scaleInPlace(10)
+
+// Create basic world
+var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 2, -1), scene)
+
+//camera.setTarget(BABYLON.Vector3.Zero())
+camera.attachControl(canvas, true)
+camera.minZ = 0;
+var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
+light.intensity = 0.7
+
+// Setup vr
+var vrHelper = scene.createDefaultVRExperience({floorMeshes: [env.ground]})
+vrHelper.raySelectionPredicate = (mesh:BABYLON.AbstractMesh):boolean=>{
+    return mesh.isVisible && mesh.isPickable;
+}
+
+var win:any = window
+win.shell = new Shell(scene, vrHelper);
+    
