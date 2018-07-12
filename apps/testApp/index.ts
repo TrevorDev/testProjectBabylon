84c678ffdb45b3e52bd8b63a94a4ea3abc3ec8cc
Ruiqing Rachel Liu <ruiqliu@microsoft.com>
2018-07-11 22:02:51 -0700

    chatting app start

--- apps/testApp/index.ts
+++ apps/testApp/index.ts
@@ -40,7 +40,78 @@ shell.registerApp({
         button.cornerRadius = 200
         button.thickness = 20
         button.onPointerClickObservable.add(()=>{
-            console.log("hit")
+            console.log("hit2")
+            var myPlane = BABYLON.MeshBuilder.CreatePlane("myPlane", {width: 3, height: 2}, scene);
+            myPlane.position.z = 5;
+            var animationBox1 = new BABYLON.Animation("myAnimation", "scaling.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+            var animationBox2 = new BABYLON.Animation("myAnimation", "scaling.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+            var animationBox3 = new BABYLON.Animation("myAnimation", "scaling.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+            var keys = []; 
+
+            //At the animation key 0, the value of scaling is "1"
+            keys.push({
+                frame: 0,
+                value: 1
+            });
+
+            //At the animation key 20, the value of scaling is "0.2"
+            keys.push({
+                frame: 5,
+                value: 1.1
+            });
+
+            //At the animation key 100, the value of scaling is "1"
+            keys.push({
+                frame: 15,
+                value: 1
+            });
+            animationBox1.setKeys(keys);
+            animationBox2.setKeys(keys);
+            animationBox3.setKeys(keys);
+
+            myPlane.animations = [];
+            myPlane.animations.push(animationBox1);
+            myPlane.animations.push(animationBox2);
+            myPlane.animations.push(animationBox3);
+            var newAnimation = scene.beginAnimation(myPlane, 0, 15, true);
+            //var pickResult = scene.pick(scene.pointerX, scene.pointerY);
+            scene.onPointerDown = function (evt, pickResult) {
+                // if the click hits the ground object, we change the impact position
+
+                if (pickResult.pickedMesh.id === "myPlane") {
+                    newAnimation.pause();
+                    myPlane.dispose();
+                    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+                    var rectangle = new BABYLON.GUI.Rectangle("rect");
+                    rectangle.top = "-100px";
+                    rectangle.background = "black";
+                    rectangle.color = "yellow";
+                    rectangle.width = "200px";
+                    rectangle.height = "40px";
+                    advancedTexture.addControl(rectangle);
+
+                    var text1 = new BABYLON.GUI.TextBlock("text1");
+                    text1.fontFamily = "Helvetica";
+                    text1.textWrapping = true;
+                    text1.text = "🤣: Hello there!";
+                    text1.color = "white";
+                    text1.fontSize = 20;
+                    rectangle.addControl(text1);    
+
+                    var advancedTexture2 = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+                    var input = new BABYLON.GUI.InputText();
+                    input.width = 0.2;
+                    input.maxWidth = 0.2;
+                    input.height = "40px";
+                    input.text = "";
+                    input.color = "blue";
+                    input.background = "white";
+                    advancedTexture2.addControl(input);  
+                }
+            };
         })
         guiPanel.addControl(button)
 
