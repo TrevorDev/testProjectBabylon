1a76dd126655fe2356519c7cb57b7efbdccfdfe6
Trevor Baron <trevorjbaron@gmail.com>
2018-07-23 10:13:04 -0700

    update starter app

--- apps/testApp/index.ts
+++ apps/testApp/index.ts
@@ -1,5 +1,5 @@
 import { VRExperienceHelper } from "babylonjs";
-
+import {Stage} from "../../src/stage"
 var shell:any = (<any>window).shell
 
 var makeNotPickable = (mesh:BABYLON.AbstractMesh)=>{
@@ -16,7 +16,7 @@ shell.registerApp({
         var scene = windowAnchor.getScene();
 
         // Load gltf model and add to scene
-        var container = await BABYLON.SceneLoader.LoadAssetContainerAsync("https://raw.githubusercontent.com/TrevorDev/gltfModels/master/facebook.glb", "", this.scene)
+        var container = await BABYLON.SceneLoader.LoadAssetContainerAsync("https://raw.githubusercontent.com/TrevorDev/gltfModels/master/facebook.glb", "", scene)
         var loadedModel = container.createRootMesh()
         makeNotPickable(loadedModel) // This needs to be done on large models to save on perf when doing ray collisions from controllers
         loadedModel.position.y = 2
@@ -25,15 +25,15 @@ shell.registerApp({
         loadedModel.parent = windowAnchor
 
         // Create GUI button
-        var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 0.2, height: 0.2}, this.scene)
+        var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 0.2, height: 0.2}, scene)
         plane.position.y= 1
         plane.parent = windowAnchor // set windowAnchor as parent
-        var guiTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane)
+        var guiTexture = Stage.GUI.AdvancedDynamicTexture.CreateForMesh(plane)
         guiTexture
-        var guiPanel = new BABYLON.GUI.StackPanel()  
+        var guiPanel = new Stage.GUI.StackPanel()  
         guiPanel.top = "0px"
         guiTexture.addControl(guiPanel)
-        var button = BABYLON.GUI.Button.CreateSimpleButton("", "Click 🤣")
+        var button = Stage.GUI.Button.CreateSimpleButton("", "Click 🤣")
         button.fontSize = 300
         button.color = "white"
         button.background = "#4AB3F4"
