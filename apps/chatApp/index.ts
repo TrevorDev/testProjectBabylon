b7fa9309c5931fa98cfe5d9553e3b5c5cb3b64e4
Ruiqing Rachel Liu <ruiqliu@microsoft.com>
2018-07-24 15:45:36 -0700

    more on chat app

--- apps/chatApp/index.ts
+++ apps/chatApp/index.ts
@@ -3,22 +3,7 @@ import {Stage} from "../../src/stage"
 
 var shell:any = (<any>window).shell
 
-// pop animation
-var animationBox1 = new BABYLON.Animation("myAnimation", "scaling.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-var animationBox2 = new BABYLON.Animation("myAnimation", "scaling.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-var animationBox3 = new BABYLON.Animation("myAnimation", "scaling.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 
-var keys = [{
-    frame: 0,
-    value: 1},{
-    frame: 5,
-    value: 1.1},{
-    frame: 15,
-    value: 1}]; 
-
-animationBox1.setKeys(keys);
-animationBox2.setKeys(keys);
-animationBox3.setKeys(keys);
 
 
 
@@ -32,14 +17,68 @@ shell.registerApp({
         let container = await BABYLON.SceneLoader.LoadAssetContainerAsync("https://raw.githubusercontent.com/rachyliu/assets/master/ChatApp.glb", "", scene)
         
         let loadedModel = container.createRootMesh();
-        let popAnimation = new BABYLON.AnimationGroup("popGroup", scene);
+        let creatPopAnimation = function (targetMesh){
+            // pop animation
+                var animationBox1 = new BABYLON.Animation("myAnimation", "scaling.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+                var animationBox2 = new BABYLON.Animation("myAnimation", "scaling.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+                var animationBox3 = new BABYLON.Animation("myAnimation", "scaling.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+            
+                var keys = [{
+                    frame: 0,
+                    value: 1},{
+                    frame: 5,
+                    value: 1.1},{
+                    frame: 15,
+                    value: 1}]; 
+            
+                animationBox1.setKeys(keys);
+                animationBox2.setKeys(keys);
+                animationBox3.setKeys(keys);
+
+                let popAnimation = new BABYLON.AnimationGroup("popGroup"+targetMesh.name, scene);
+                popAnimation.addTargetedAnimation(animationBox1, targetMesh);
+                popAnimation.addTargetedAnimation(animationBox2, targetMesh);
+                popAnimation.addTargetedAnimation(animationBox3, targetMesh);
+                scene.addAnimationGroup(popAnimation);
+            }
+        
+
+        let hoverEffect = function(target){
+            target.actionManager = new BABYLON.ActionManager(scene);
+
+            target.actionManager.registerAction(
+                new BABYLON.InterpolateValueAction(
+                    {
+                        trigger: BABYLON.ActionManager.OnPointerOverTrigger,
+                        parameter: target
+                    },
+                    target,
+                    "scaling",
+                    new BABYLON.Vector3(1.2, 1.2, 1.2),
+                    50
+                )
+            );
+            target.actionManager.registerAction(
+                new BABYLON.InterpolateValueAction(
+                    {
+                        trigger: BABYLON.ActionManager.OnPointerOutTrigger,
+                        parameter: target
+                    },
+                    target,
+                    "scaling",
+                    new BABYLON.Vector3(1, 1, 1),
+                    50
+                )
+            );
+        }
+
         /**
          * Creates a contact list
          */
         let createContactList = function (){
-            var Contacts = ["https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople.png", 
+            var Contacts = ["https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople2.png", 
             "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople1.png", 
-            "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople2.png", 
+            "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople4.png", 
             "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople3.png"];
 
             for (var i in Contacts){
@@ -61,68 +100,57 @@ shell.registerApp({
 
                 myPlane.visibility = 0;
 
-                myPlane.actionManager = new BABYLON.ActionManager(scene);
-                myPlane.actionManager.registerAction(
-                    new BABYLON.InterpolateValueAction(
-                        {
-                            trigger: BABYLON.ActionManager.OnPointerOverTrigger,
-                            parameter: myPlane
-                        },
-                        myPlane,
-                        "scaling",
-                        new BABYLON.Vector3(1.2, 1.2, 1.2),
-                        50
-                    )
-                );
-                myPlane.actionManager.registerAction(
-                    new BABYLON.InterpolateValueAction(
-                        {
-                            trigger: BABYLON.ActionManager.OnPointerOutTrigger,
-                            parameter: myPlane
-                        },
-                        myPlane,
-                        "scaling",
-                        new BABYLON.Vector3(1, 1, 1),
-                        50
-                    )
-                );
+                hoverEffect(myPlane);
 
-                myPlane.actionManager.registerAction(
-                    new BABYLON.ExecuteCodeAction(
-                        {
-                            trigger: BABYLON.ActionManager.OnPointerOverTrigger,
-                            parameter: myPlane
-                        },
-                        function () { console.log('r button was pressed'); }
-                    )
-                );
+                creatPopAnimation(myPlane);
 
                 createDialogBox(myPlane);
 
-                var option1 = BABYLON.MeshBuilder.CreatePlane("listAction1."+i, {width: 0.25, height: 0.25}, scene);
+                var option1 = BABYLON.MeshBuilder.CreatePlane("listAction1." + myPlane.name, {width: 0.25, height: 0.25}, scene);
                 //myPlane.dispose();
                 option1.position.z = 0
                 option1.position.x = 0.6
                 option1.position.y = 0;
                 option1.parent = myPlane;
                 option1.visibility = 0;
+                var guiTexture = Stage.GUI.AdvancedDynamicTexture.CreateForMesh(option1)
 
-                var option2 = BABYLON.MeshBuilder.CreatePlane("listAction1."+i, {width: 0.25, height: 0.25}, scene);
+                var guiPanel = new Stage.GUI.StackPanel()  
+                guiPanel.top = "0px"
+                guiTexture.addControl(guiPanel)
+                var close = Stage.GUI.Button.CreateImageOnlyButton("chat", "https://raw.githubusercontent.com/rachyliu/assets/master/chatIcon.png");
+                close.fontSize = 300
+                close.color = "white"
+                close.cornerRadius = 500
+
+                close.onPointerClickObservable.add(()=>{
+                    console.log("1");
+                })
+                guiPanel.addControl(close);
+                hoverEffect(option1);
+
+
+                var option2 = BABYLON.MeshBuilder.CreatePlane("listAction1." + myPlane.name, {width: 0.25, height: 0.25}, scene);
                 //myPlane.dispose();
                 option2.position.z = 0
                 option2.position.x = 0.9
                 option2.position.y = 0;
                 option2.parent = myPlane;
                 option2.visibility = 0;
+                var guiTexture = Stage.GUI.AdvancedDynamicTexture.CreateForMesh(option2)
 
-                var option3 = BABYLON.MeshBuilder.CreatePlane("listAction1."+i, {width: 0.25, height: 0.25}, scene);
-                //myPlane.dispose();
-                option3.position.z = 0
-                option3.position.x = 1.2
-                option3.position.y = 0;
-                option3.parent = myPlane;
-                option3.visibility = 0;
-
+                var guiPanel = new Stage.GUI.StackPanel()  
+                guiPanel.top = "0px"
+                guiTexture.addControl(guiPanel)
+                var close = Stage.GUI.Button.CreateImageOnlyButton("video", "https://raw.githubusercontent.com/rachyliu/assets/master/videoIcon.png");
+                close.fontSize = 300
+                close.color = "white"
+                close.cornerRadius = 500
+                close.onPointerClickObservable.add(()=>{
+                    console.log("2");
+                })
+                guiPanel.addControl(close);
+                hoverEffect(option2);
             }
         }
 
@@ -179,10 +207,8 @@ shell.registerApp({
         loadedModel.position.y = 1.5
         loadedModel.position.x = 2
   
-        popAnimation.addTargetedAnimation(animationBox1, loadedModel);
-        popAnimation.addTargetedAnimation(animationBox2, loadedModel);
-        popAnimation.addTargetedAnimation(animationBox3, loadedModel);
-        scene.addAnimationGroup(popAnimation);
+        creatPopAnimation(loadedModel);
+
         scene.addMesh(loadedModel, true)
         // Any mesh created MUST have the windowAnchor as it's parent
         windowAnchor.addChild(loadedModel);
@@ -202,6 +228,7 @@ shell.registerApp({
                 ||evt.pickInfo.pickedMesh.id === "name2" 
                 ||evt.pickInfo.pickedMesh.id === "name3" )) {
                 console.log("click");
+                scene.getAnimationGroupByName("popGroup"+evt.pickInfo.pickedMesh.id).stop();
                 scene.getMeshByName("dialogBox" + evt.pickInfo.pickedMesh.id).visibility = 1;
                 scene.getMeshByName("closePlane" + evt.pickInfo.pickedMesh.id).visibility = 1;
                 // var advancedTexture3 = Stage.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
@@ -240,9 +267,9 @@ shell.registerApp({
                 // input.linkOffsetX = 400
             }
             if ( evt.type==BABYLON.PointerEventTypes.POINTERUP && evt.pickInfo.pickedMesh && evt.pickInfo.pickedMesh.id === "node_id32") {
-                popAnimation.stop();
+                scene.getAnimationGroupByName("popGroupassetContainerRootMesh").stop();
                 let visibility;
-                listMesh.getChildMeshes(true).forEach(element => {
+                listMesh.getChildMeshes().forEach(element => {
                     element.visibility = element.visibility === 1 ? 0:1;
                     visibility = element.visibility;
 
@@ -267,7 +294,12 @@ shell.registerApp({
                     trigger: BABYLON.ActionManager.OnKeyUpTrigger,
                     parameter: 'q'
                 },
-                function () { popAnimation.play(true); }
+                function () { 
+                    scene.getAnimationGroupByName("popGroupname0").play(true);
+                    scene.getAnimationGroupByName("popGroupname1").play(true);
+                    scene.getAnimationGroupByName("popGroupassetContainerRootMesh").play(true);
+
+                }
             )
         );
 
