76e00e3478338eec8368eb9c455cc158e4534baa
Kelly Albano <brandonlico@gmail.com>
2018-07-24 14:14:48 -0700

    Getting shit working

--- index.ts
+++ index.ts
@@ -1,61 +1,18 @@
 import { Stage } from './src/stage'
-import * as SDK from 'microsoft-speech-browser-sdk'
-import {Recognizer} from "microsoft-speech-browser-sdk";
 
 class App {
     constructor(public name:string, public iconUrl:string, public launch:Function, public dispose:Function){
 
     }
 }
-
-
-function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
-    let recognizerConfig = new SDK.RecognizerConfig(
-        new SDK.SpeechConfig(
-            new SDK.Context(
-                new SDK.OS(navigator.userAgent, "Browser", null),
-                new SDK.Device("SpeechSample", "SpeechSample", "1.0.00000"))),
-        recognitionMode, // SDK.RecognitionMode.Interactive  (Options - Interactive/Conversation/Dictation)
-        language, // Supported languages are specific to each recognition mode Refer to docs.
-        format); // SDK.SpeechResultFormat.Simple (Options - Simple/Detailed)
-
-    // Alternatively use SDK.CognitiveTokenAuthentication(fetchCallback, fetchOnExpiryCallback) for token auth
-    let authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
-
-    console.log(SDK.Recognizer);
-    return SDK.Recognizer.Create(recognizerConfig, authentication);
-}
-
-
-function RecognizerStop(SDK, recognizer) {
-    // recognizer.AudioSource.Detach(audioNodeId) can be also used here. (audioNodeId is part of ListeningStartedEvent)
-    recognizer.AudioSource.TurnOff();
-}
-
-
 class Shell {
-
     private apps:Array<App> = []
-    constructor(public scene:BABYLON.Scene, public vrHelper:BABYLON.VRExperienceHelper, public temp_recognizer:SDK.Recognizer){
-
-        this.temp_recognizer.Recognize((event) => {
-
-            if (event instanceof SDK.RecognitionEndedEvent)
-            {
-                console.log(JSON.stringify(event)); // Debug information
-            }
-            else {
-                console.log('didnt invoke');
-            }
-        })
-        .On(() => {
-            // The request succeeded. Nothing to do here.
-        },
-        (error) => {
-            console.error(error);
-        });
+    private x: number = 1
+    constructor(public scene:BABYLON.Scene, public vrHelper:BABYLON.VRExperienceHelper){}
+    positionSphere = (sphere: any) => {
+        sphere.position.x = this.x;
+        this.x += 1;
     }
-    
     registerApp = (app:App)=>{
         var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 0.5, this.scene)
 
@@ -132,13 +89,7 @@ class Shell {
         })
 
     }
-
 }
-
-
-
-
-
 
 // Initialize full screen rendering
 var stage = new Stage()
@@ -165,7 +116,5 @@ vrHelper.raySelectionPredicate = (mesh:BABYLON.AbstractMesh):boolean=>{
 }
         
 var win:any = window
-
-var recognizer = RecognizerSetup(SDK, SDK.RecognitionMode.Interactive, "en-us", SDK.SpeechResultFormat.Simple, "blah")
-win.shell = new Shell(scene, vrHelper, recognizer);
+win.shell = new Shell(scene, vrHelper);
     
