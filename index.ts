6cec886b4272d1edf1ea426e6b5fcd02ded796c6
alexstelea <alex.stelea@gmail.com>
2018-07-24 14:25:14 -0700

    added phone gltf and app launcher menu

--- index.ts
+++ index.ts
@@ -5,6 +5,14 @@ class App {
 
     }
 }
+
+var makeNotPickable = (mesh:BABYLON.AbstractMesh)=>{
+    mesh.isPickable = false;
+    mesh.getChildMeshes().forEach((m)=>{
+        makeNotPickable(m)
+    })
+}
+
 class Shell {
     private apps:Array<App> = []
     private x: number = 1
@@ -44,7 +52,7 @@ class Shell {
         var state = 0;
         let launched = false;
         this.scene.onPointerObservable.add((e)=>{
-            if (e.type == BABYLON.PointerEventTypes.POINTERDOWN) {
+            if (e.type == BABYLON.PointerEventTypes.POINTERDOUBLETAP) {
                 if(e.pickInfo.pickedMesh == sphere) {
                     if(!launched){
                         this.apps.push(app)
@@ -79,30 +87,101 @@ class Shell {
     }
 }
 
-// Initialize full screen rendering
-var stage = new Stage()
-var scene = stage.scene
-var canvas = stage.engine.getRenderingCanvas()
+var main = async () => {
+    // Initialize full screen rendering
+    var stage = new Stage()
+    var scene = stage.scene
+    var canvas = stage.engine.getRenderingCanvas()
 
-var env = scene.createDefaultEnvironment({})
-env.setMainColor(BABYLON.Color3.FromHexString("#7f8c8d"))
-env.skybox.scaling.scaleInPlace(10)
+    var env = scene.createDefaultEnvironment({})
+    env.setMainColor(BABYLON.Color3.FromHexString("#7f8c8d"))
+    env.skybox.scaling.scaleInPlace(10)
 
-// Create basic world
-var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(1, 1, -4), scene)
+    // Create basic world
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(1, 1, -4), scene)
 
-//camera.setTarget(BABYLON.Vector3.Zero())
-camera.attachControl(canvas, true)
-camera.minZ = 0;
-var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
-light.intensity = 0.7
+    //camera.setTarget(BABYLON.Vector3.Zero())
+    camera.attachControl(canvas, true)
+    camera.minZ = 0;
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
+    light.intensity = 0.7
 
-// Setup vr
-var vrHelper = scene.createDefaultVRExperience({floorMeshes: [env.ground]})
-vrHelper.raySelectionPredicate = (mesh:BABYLON.AbstractMesh):boolean=>{
-    return mesh.isVisible && mesh.isPickable;
-}
+    // Setup vr
+    var vrHelper = scene.createDefaultVRExperience({floorMeshes: [env.ground]})
+    vrHelper.raySelectionPredicate = (mesh:BABYLON.AbstractMesh):boolean=>{
+        return mesh.isVisible && mesh.isPickable;
+    }
         
-var win:any = window
-win.shell = new Shell(scene, vrHelper);
+    var win:any = window
+    win.shell = new Shell(scene, vrHelper);
+
+    // Model taken from https://poly.google.com/view/3oGDGMrc6rm
+    // CC-BY for Google as the content creator
+    // this is the 3D Phone model that will be the app launcher device
+    var phoneContainer = await BABYLON.SceneLoader.LoadAssetContainerAsync("/public/tablet/", "1327 iPhone.gltf", scene)
+    var loadedPhone = phoneContainer.createRootMesh()
+
+    // scaling the phone model to be smaller & rotated
+    loadedPhone.scaling = new BABYLON.Vector3(0.06,0.06,0.06);
+    loadedPhone.rotation.x = Math.PI
+    loadedPhone.rotation.y = Math.PI / 2
+    loadedPhone.rotation.z = Math.PI / -2
+
+    // performance optimization
+    makeNotPickable(loadedPhone)
     
+    // add phone to mesh
+    scene.addMesh(loadedPhone, true)
+
+    // menu launcher plane
+    var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 1.25, height:2}, scene);
+
+    plane.position.z = -0.2;
+
+    var advancedTexture = Stage.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+    var buttons = []
+    
+    for (var i = 0; i < 6; i++) {
+        var button = Stage.GUI.Button.CreateSimpleButton("button" + i, "App " + i);
+        button.width = 1;
+        button.height = 1;
+        button.color = "white";
+        button.fontSize = 50;
+        button.background = "green";
+        button.paddingLeft = "3%";
+        button.paddingRight = "3%";
+        button.paddingBottom = "3%";
+        
+        button.onPointerUpObservable.add(function() {
+            alert("you launched app");
+        });
+
+        buttons.push(button)
+    }
+
+    // grid for icons
+    var grid = new Stage.GUI.Grid(); 
+    grid.addColumnDefinition(0.5);
+    grid.addColumnDefinition(0.5);
+    grid.addRowDefinition(0.3);
+    grid.addRowDefinition(0.3);
+    grid.addRowDefinition(0.3);
+
+    grid.addControl(buttons[0], 0, 0);   
+    grid.addControl(buttons[1], 0, 1);
+    grid.addControl(buttons[2], 1, 0);
+    grid.addControl(buttons[3], 1, 1);
+    grid.addControl(buttons[4], 2, 0);
+    grid.addControl(buttons[5], 2, 1);
+    
+    advancedTexture.addControl(grid);
+
+    // parent menu mesh that holds both the phone and ui
+    var parentMenuMesh = new BABYLON.Mesh('parentMesh1', scene)
+    
+    parentMenuMesh.addChild(plane)
+    parentMenuMesh.addChild(loadedPhone)
+
+    // vrController.addChild(parentMenuMesh))
+}
+main()
