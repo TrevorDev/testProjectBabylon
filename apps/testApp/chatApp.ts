79ad72ec835eaace9dafaca7cbb6aa19e4508008
Ruiqing Rachel Liu <ruiqliu@microsoft.com>
2018-07-23 17:35:04 -0700

    update contact list

--- apps/testApp/chatApp.ts
+++ apps/testApp/chatApp.ts
@@ -1,4 +1,4 @@
-import { VRExperienceHelper } from "babylonjs";
+import { VRExperienceHelper, ActionManager } from "babylonjs";
 
 var shell:any = (<any>window).shell
 
@@ -25,7 +25,6 @@ animationBox1.setKeys(keys);
 animationBox2.setKeys(keys);
 animationBox3.setKeys(keys);
 
-var popAniamtion = new BABYLON.AnimationGroup("popGroup");
 
 
 shell.registerApp({
@@ -34,25 +33,64 @@ shell.registerApp({
         // Get scene
         var scene = windowAnchor.getScene();
         // Load gltf model and add to scene
-        scene.addAnimationGroup(popAniamtion);
+        var popAniamtion = new BABYLON.AnimationGroup("popGroup");
+        
+        
         var container = await BABYLON.SceneLoader.LoadAssetContainerAsync("https://raw.githubusercontent.com/rachyliu/assets/master/ChatApp.glb", "", this.scene)
         
-        var loadedModel = container.createRootMesh()
+        var loadedModel = container.createRootMesh();
 
-        var b= new BABYLON.PointerDragBehavior({dragPlaneNormal: new BABYLON.Vector3(0,1,0)})
+        /**
+         * Creates a contact list
+         */
+        var createContactList = function (){
+            var Contacts = ["https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople.png", 
+            "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople1.png", 
+            "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople2.png", 
+            "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople3.png"];
+
+            for (var i in Contacts){
+                var myPlane = BABYLON.MeshBuilder.CreatePlane("name"+i, {width: 0.8, height: 0.25}, scene);
+                //myPlane.dispose();
+                myPlane.position.z = 0
+                myPlane.position.x = 0
+                myPlane.position.y = 0.5 +0.3*(+i);
+                myPlane.parent = listMesh;
+                var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+                // myMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0);
+
+                myPlane.material = myMaterial;//(+i === Contacts.length-1)? myMaterial:null;
+
+                let texture  = new BABYLON.Texture(Contacts[i], scene);
+                texture.hasAlpha = true;
+                myMaterial.diffuseTexture = texture;
+                myMaterial.emissiveTexture = texture;
+
+                myPlane.visibility = 0;
+            }
+        }
+
+        var listMesh = new BABYLON.Mesh("contactList", scene);
+        listMesh.parent = loadedModel;
+        
+        createContactList();
+
+        var b = new BABYLON.PointerDragBehavior({dragPlaneNormal: new BABYLON.Vector3(0,1,0)})
         loadedModel.addBehavior(b)
         //makeNotPickable(loadedModel) // This needs to be done on large models to save on perf when doing ray collisions from controllers
         loadedModel.position.z = 2
         loadedModel.position.y = 2
         loadedModel.position.x = - 2
-
+  
         popAniamtion.addTargetedAnimation(animationBox1, loadedModel);
         popAniamtion.addTargetedAnimation(animationBox2, loadedModel);
         popAniamtion.addTargetedAnimation(animationBox3, loadedModel);
-        
+        scene.addAnimationGroup(popAniamtion);
         scene.addMesh(loadedModel, true)
         // Any mesh created MUST have the windowAnchor as it's parent
         loadedModel.parent = windowAnchor
+
+        //var chatActions = new ActionManager(scene);
 
         // Create GUI button
         var plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 0.2, height: 0.2}, this.scene)
@@ -86,6 +124,27 @@ shell.registerApp({
 
         })
         guiPanel.addControl(button)
+        
+        
+        // Conditions
+        //sphere.actionManager = new BABYLON.ActionManager(scene);
+        //var condition1 = new BABYLON.StateCondition(sphere.actionManager, redLight, "off");
+        //var condition2 = new BABYLON.StateCondition(sphere.actionManager, redLight, "on");
+    
+        //sphere.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnLeftPickTrigger, camera, "alpha", 0, 500, condition1));
+        //sphere.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnLeftPickTrigger, camera, "alpha", Math.PI, 500, condition2));
+    
+        // Over/Out
+        //var makeOverOut = function (mesh) {
+        //    mesh.actionManager.registerAction(new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh.material, "emissiveColor", mesh.material.emissiveColor));
+        //    mesh.actionManager.registerAction(new BABYLON.SetValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh.material, "emissiveColor", BABYLON.Color3.White()));
+            //mesh.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOutTrigger, mesh, "scaling", new BABYLON.Vector3(1, 1, 1), 150));
+            //mesh.actionManager.registerAction(new BABYLON.InterpolateValueAction(BABYLON.ActionManager.OnPointerOverTrigger, mesh, "scaling", new BABYLON.Vector3(1.1, 1.1, 1.1), 150));
+        //}
+
+        
+    
+
         scene.onPointerDown = function (evt, pickResult) {
             console.log(pickResult.pickedMesh.id);
             // if the click hits the ground object, we change the impact position
@@ -127,57 +186,20 @@ shell.registerApp({
             }
             if (pickResult.pickedMesh.id === "node_id32") {
                 popAniamtion.stop();
+                listMesh.getChildMeshes().forEach(element => {
+                    element.visibility = element.visibility === 1 ? 0:1;
 
-                var Contacts = ["https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople.png", 
-                    "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople1.png", 
-                    "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople2.png", 
-                    "https://raw.githubusercontent.com/rachyliu/assets/master/contactPeople3.png"];
-                var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-
-                for (var i in Contacts){
-                    var myPlane = BABYLON.MeshBuilder.CreatePlane("name"+i, {width: 0.8, height: 0.25}, scene);
-                    //myPlane.dispose();
-                    myPlane.position.z = 0
-                    myPlane.position.x = 0
-                    myPlane.position.y = 0.5 +0.3*(+i);
-                    myPlane.parent = loadedModel;
-                    var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
-                    // myMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0);
-
-                    myPlane.material = myMaterial;//(+i === Contacts.length-1)? myMaterial:null;
-                    console.log(i);
-                    /*var rectangle = new BABYLON.GUI.Rectangle("rect");
-                    rectangle.top = "-100px";
-                    rectangle.background = (+i === Contacts.length-1)? "red":"black";
-                    rectangle.color = "yellow";
-                    rectangle.width = "200px";
-                    rectangle.height = "40px";
-                    advancedTexture.addControl(rectangle);*/
-                    // var textureGround = new BABYLON.DynamicTexture("dynamic texture", {width:512, height:256}, scene, false);
-                    let texture  = new BABYLON.Texture(Contacts[i], scene);
-                    texture.hasAlpha = true;
-                    myMaterial.diffuseTexture = texture;
-                    myMaterial.emissiveTexture = texture;
-
-
-                    /*var name = new BABYLON.GUI.TextBlock("name");
-                    name.fontFamily = "Helvetica";
-                    name.textWrapping = true;
-                    name.text = Contacts[i];
-                    name.color = "white";
-                    name.fontSize = 20;
-                    rectangle.addControl(name);   
-                    rectangle.linkWithMesh(loadedModel);   
-                    rectangle.linkOffsetY = -100-50*(+i);
-                    rectangle.linkOffsetX = -200 */
-
-                }
-
+                });
                 
             }
         };
+
+
+
     }, 
     dispose: async ()=>{
 
     }
+
+
 })
