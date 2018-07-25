9f90e57ebe089c35a1eaaa9bcb0d0279c23f0280
Kelly Albano <brandonlico@gmail.com>
2018-07-24 17:05:09 -0700

    Open ended speech detection

--- index.ts
+++ index.ts
@@ -1,4 +1,81 @@
 import { Stage } from './src/stage'
+import * as SDK from 'microsoft-speech-browser-sdk'
+
+function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
+    let recognizerConfig = new SDK.RecognizerConfig(
+        new SDK.SpeechConfig(
+            new SDK.Context(
+                new SDK.OS(navigator.userAgent, "Browser", null),
+                new SDK.Device("SpeechSample", "SpeechSample", "1.0.00000"))),
+        recognitionMode, // SDK.RecognitionMode.Interactive  (Options - Interactive/Conversation/Dictation)
+        language, // Supported languages are specific to each recognition mode Refer to docs.
+        format); // SDK.SpeechResultFormat.Simple (Options - Simple/Detailed)
+
+    // Alternatively use SDK.CognitiveTokenAuthentication(fetchCallback, fetchOnExpiryCallback) for token auth
+    let authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
+ 
+    return SDK.CreateRecognizer(recognizerConfig, authentication);
+}
+
+function RecognizerStart(SDK, recognizer) {
+    recognizer.Recognize((event) => {
+        /*
+            Alternative syntax for typescript devs.
+            if (event instanceof SDK.RecognitionTriggeredEvent)
+        */
+        switch (event.Name) {
+            case "RecognitionTriggeredEvent" :
+                //UpdateStatus("Initializing");
+                break;
+            case "ListeningStartedEvent" :
+                //UpdateStatus("Listening");
+                break;
+            case "RecognitionStartedEvent" :
+                //UpdateStatus("Listening_Recognizing");
+                break;
+            case "SpeechStartDetectedEvent" :
+                //UpdateStatus("Listening_DetectedSpeech_Recognizing");
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechHypothesisEvent" :
+                //UpdateRecognizedHypothesis(event.Result.Text);
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechFragmentEvent" :
+                //UpdateRecognizedHypothesis(event.Result.Text);
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechEndDetectedEvent" :
+                //OnSpeechEndDetected();
+                //UpdateStatus("Processing_Adding_Final_Touches");
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechSimplePhraseEvent" :
+                console.log(JSON.stringify(event.Result));
+                //UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+                break;
+            case "SpeechDetailedPhraseEvent" :
+                //UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+                break;
+            case "RecognitionEndedEvent" :
+                //OnComplete();
+                //UpdateStatus("Idle");
+                console.log(JSON.stringify(event)); // Debug information
+                break;
+        }
+    })
+    .On(() => {
+        // The request succeeded. Nothing to do here.
+    },
+    (error) => {
+        console.error(error);
+    });
+}
+
+function RecognizerStop(SDK, recognizer) {
+    // recognizer.AudioSource.Detach(audioNodeId) can be also used here. (audioNodeId is part of ListeningStartedEvent)
+    recognizer.AudioSource.TurnOff();
+}
 
 class App {
     constructor(public name:string, public iconUrl:string, public launch:Function, public dispose:Function){
@@ -114,7 +191,11 @@ var vrHelper = scene.createDefaultVRExperience({floorMeshes: [env.ground]})
 vrHelper.raySelectionPredicate = (mesh:BABYLON.AbstractMesh):boolean=>{
     return mesh.isVisible && mesh.isPickable;
 }
-        
+
 var win:any = window
 win.shell = new Shell(scene, vrHelper);
-    
+   
+var recognizer = RecognizerSetup(SDK, SDK.RecognitionMode.Conversation, "en-us", SDK.SpeechResultFormat.Simple, "92069ee289b84e5594a9564ab77ed2ba");
+RecognizerStart(SDK, recognizer);
+
+console.log(win.shell.apps)
