c8085a4769fcaac4744814bede897be94f7ebce9
Trevor Baron <trevorjbaron@gmail.com>
2018-08-06 00:12:02 -0700

    adding express

--- server.ts
+++ server.ts
@@ -1,6 +1,13 @@
-var world = async()=>{
-   return "world"; 
-}
-(async()=>{
-    console.log("hello "+await world())
-})()
+import * as express from "express"
+import * as http from "http"
+
+var app = express()
+app.set('view engine', 'pug')
+app.use("/public", express.static("public"))
+app.get('/', function (req, res) {
+    var app = req.query.app ? req.query.app : 'testApp'
+    res.render('app', { app: app })
+})
+
+var server = http.createServer(app)
+server.listen(3000)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 // https://webpack.js.org/guides/typescript/
-
+var nodeExternals = require('webpack-node-externals');
+var glob = require("glob")
 const path = require('path');
 
-module.exports = {
+
+module.exports = [{
   entry: './server.ts',
+  mode: 'development',
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+},
+{
+  entry: 
+    // every ts file in the apps dir
+    // TODO: webpack needs to be restarted when adding new files
+    glob.sync("apps/*.ts").reduce((p, c)=>{
+      p[path.basename(c).split(".")[0]] = "./"+c;
+      return p;
+    }, {})
+  ,
   mode: 'development',
   module: {
     rules: [
@@ -18,7 +49,8 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/dist')
   }
-};
+}
+];
