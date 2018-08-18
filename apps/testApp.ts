570b4ae04d748fd36d8e363ccce4664ea9d45b32
Trevor Baron <trevorjbaron@gmail.com>
2018-08-18 14:08:59 -0700

    cleanup

--- apps/testApp.ts
+++ apps/testApp.ts
@@ -1,8 +1,5 @@
 import * as BABYLON from 'babylonjs'
 
-
-
-
 // Get rid of margin
 document.documentElement.style["overflow"]="hidden"
 document.documentElement.style.overflow ="hidden"
@@ -31,34 +28,19 @@ var scene = new BABYLON.Scene(engine)
 engine.runRenderLoop(()=>{
     scene.render()
 })
-
 window.addEventListener("resize", ()=> {
     engine.resize()
 })
 
-var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
+camera.setTarget(BABYLON.Vector3.Zero())
+camera.attachControl(canvas, true)
+var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
+light.intensity = 0.7
 
-// This targets the camera to scene origin
-camera.setTarget(BABYLON.Vector3.Zero());
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene)
+sphere.position.y = 1
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
-
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-// Move the sphere upward 1/2 its height
-sphere.position.y = 1;
-
-// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 1, scene);
+var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 1, scene)
 ground.material = new BABYLON.StandardMaterial("",scene)
-ground.material.wireframe = true;
-
-console.log("hello world2")
